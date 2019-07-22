b64b08eb46640c8d5b6473e5afa325b0643dbfbf
hasim imahdudin <hasim.imahdudin@bytedance.com>
2019-07-22 21:04:04 +0700

    validation

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import { Row, Col } from 'antd';
-import Input from 'antd/es/input';
-import Button from 'antd/es/button';
+import {
+  Form,
+  Input,
+  Select,
+  message,
+  Modal,
+  Table,
+  Button,
+  Cascader,
+  Row,
+  Col,
+  Tooltip,
+  Icon,
+  InputNumber,
+} from 'antd';
 
+const FormItem = Form.Item;
+
+@Form.create()
 class TokpedTest extends React.PureComponent {
   state = {
     nominal: 0,
     sisa: 0,
     hasil: '',
+    visibleModal: false,
     hideResult: true,
+    invalidInput: [],
+    coinList: [],
   };
 
   componentDidMount() {
+    this.setState({
+      coinList: [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50],
+    });
     console.log(
       'Please contact  Me \n Hasim Imahdudin \n hasim.blogger@gmail.com, hasim.imahdudin@gmail.com \n 085642856762 :) ',
     );
   }
 
-  handleHitung = () => {
+  processCalculate = () => {
     // contoh koin 1500
-    // let result = ''
-    let originalNum = this.state.nominal;
+    const originalNum = this.state.nominal;
     let num = this.state.nominal;
-    const arr = [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50];
+    const arr = this.state.coinList;
 
     // 1000, 500
-    const coins = [];
+    const coins: any = [];
     let sumNominal = 0;
     let sisa = 0;
 
@@ -64,39 +84,94 @@ class TokpedTest extends React.PureComponent {
       sisa = originalNum - sumNominal;
     }
 
-    const countResult = coins.reduce((a, c) => ((a[c] = (a[c] || 0) + 1), a), Object.create(null));
+    const countResult = coins.reduce(
+      (a: any, c: any) => ((a[c] = (a[c] || 0) + 1), a),
+      Object.create(null),
+    );
     this.setState({
       hasil: countResult,
       sisa: sisa,
       hideResult: false,
     });
-
-    return coins;
   };
 
-  handleValidInput = (e: any) => {
-    const nominal = e.target.value;
+  handleSubmit = e => {
+    let _this = this;
+    e.preventDefault();
+    this.props.form.validateFields((err: any, values: any) => {
+      if (!err) {
+        _this.processCalculate();
+      }
+    });
+  };
+
+  inputValidator = (rules: any, value: any, callback: any) => {
+    if (value) {
+      const nominal = value;
+      if (nominal.match(/^[a-z|A-Z|0-9]+\s?Rp|\sRp.{1}$/gm)) {
+        callback('valid character in wrong position');
+      } else if (nominal.match(/([0-9])\s|,([0-9])/gm)) {
+        callback('invalid separator');
+      } else if (nominal.match(/^(Rp|RP)\s(?!\d)/gm)) {
+        callback('missing value');
+      } else {
+        this.setState({
+          hideResult: false,
+          nominal: nominal.replace(/\D/g, ''),
+        });
+        callback();
+      }
+    } else {
+      callback();
+    }
+  };
+
+  updateCoinList = (idx: any, e: any) => {
+    console.log(idx, e.target.value);
+    const value = e.target.value;
     this.setState({
-      hideResult: true,
-      nominal: nominal,
+      coinList: value,
     });
   };
 
   render() {
-    const { hasil, sisa, nominal, hideResult } = this.state;
+    const { hasil, sisa, nominal, hideResult, coinList } = this.state;
     const array = Object.keys(hasil).reverse();
+    const { getFieldDecorator } = this.props.form;
+
     return (
       <React.Fragment>
         <Row>
-          <Col span={6}>
-            <Input onKeyUp={e => this.handleValidInput(e)} />
-          </Col>
-          <Col span={2}>
-            <Button type="dashed" onClick={this.handleHitung}>
-              {' '}
-              Hitung{' '}
-            </Button>
-          </Col>
+          {coinList.map((val, idx) => {
+            return (
+              <Col span={4} key={idx}>
+                <Input defaultValue={val} onKeyUp={e => this.updateCoinList(idx, e)} />
+              </Col>
+            );
+          })}
+        </Row>
+        <Row>
+          <Form onSubmit={this.handleSubmit} className="test-web-form">
+            <Col span={6}>
+              <Form.Item>
+                {getFieldDecorator('nominal', {
+                  rules: [
+                    { required: true, message: 'Please input Nominal!' },
+                    {
+                      validator: this.inputValidator,
+                    },
+                  ],
+                })(<Input placeholder="Nominal" />)}
+              </Form.Item>
+            </Col>
+            <Col span={6} offset={1}>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Calculate
+                </Button>
+              </Form.Item>
+            </Col>
+          </Form>
         </Row>
         {!hideResult && (
           <Row>
@@ -106,11 +181,11 @@ class TokpedTest extends React.PureComponent {
                 {array.map(function(key, index) {
                   return (
                     <li key={index}>
-                      {hasil[key]} X {key}
+                      {hasil[key]} X Rp{key}
                     </li>
                   );
                 })}
-                {sisa !== 0 && <li>Sisa : {sisa} </li>}
+                {sisa !== 0 && <li>no available fraction : {sisa} </li>}
               </ul>
             </Col>
           </Row>
